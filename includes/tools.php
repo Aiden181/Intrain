b8f1f1b04ee526c6101de9ceeb052253965ceec0
s3757317 <s3757317@users.noreply.github.com>
2020-12-04 15:38:36 +0700

    completed login auth for admin and customer

--- includes/tools.php
+++ includes/tools.php
@@ -26,44 +26,103 @@ function prePrintArray( $arr, $returnAsString=false ) {
         echo $ret; 
 }
 
+// login auth variables
+$error_message = "";
+$invalid_login = false;
+
 // when a POST form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // reset login
-    if (isset($_POST['reset-login'])) {
-        unset($_SESSION['User']);
+    // ------------------------------------ //
+    // --- LOGIN AUTHENTICATION SECTION --- //
+    // ------------------------------------ //
+    // reset login or log out button is pressed
+    if (isset($_POST['reset-login']) || isset($_POST['logout'])) {
+        // remove user from session
+        if (isset($_SESSION['Customer'])) { unset($_SESSION['Customer']); }
+        // remove admin from session
+        if (isset($_SESSION['Admin'])){ unset($_SESSION['Admin']); }
     }
+
     // 'login' is in $_POST (after user presses Log In button)
     if (isset($_POST['login'])) {
-        // if username and password in $_POST are set then remove 
-        // malicious characters and assign those values to the variable
-        $username = isset($_POST['username']) ? test_input($_POST['username']) : '';
-        $password = isset($_POST['password']) ? test_input($_POST['password']) : '';
-        
-        // GET username and password from user's inputs
-        $sql = "SELECT `username`, `password` FROM `admin` WHERE username='$username' AND password=PASSWORD('$password')";
-        
-        // run mysql query and store results to $result
-        if ($result = mysqli_query($conn, $sql)) {
-            // has at least 1 result
-            if (mysqli_num_rows($result) > 0) {
-                // add user to session
-                $_SESSION['User']['username'] = $username;
+        // none of the fields are empty and have value other than NULL
+        if ((!empty($_POST['username']) && isset($_POST['username'])) && (!empty($_POST['password']) && isset($_POST['password']))) {
+            // remove malicious characters then assign those values to the variable
+            $username = test_input($_POST['username']);
+            $password = test_input($_POST['password']);
+            
+            // GET username and password from user's inputs from admin table
+            $sql = "SELECT `username`, `password` FROM `admin` WHERE username='$username' AND password=PASSWORD('$password')";
+            
+            // run mysql query and store results to $result
+            if ($result = mysqli_query($conn, $sql)) {
+                // has at least 1 result
+                if (mysqli_num_rows($result) > 0) {
+                    // add admin to session
+                    $_SESSION['Admin'] = $username;
 
-                // Free result set (free up memory)
-                mysqli_free_result($result);
+                    // Free result set (free up memory)
+                    mysqli_free_result($result);
 
-                // redirects to index.php upon successful login
-                header("location: index.php");
-                exit;
+                    // redirects to index.php upon successful login
+                    header("Location: ./index.php");
+                    exit;
+                }
+                else {
+                    $invalid_login = true;
+                    $error_message = "<span style='color: red'> Username or password is incorrect! </span>";
+                }
             }
-            // no results returned
             else {
-                $Msg = "<span> Username or password is incorrect! </span>";
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
             }
-        } else {
-            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+            
+            // GET username and password from user's inputs customer table
+            $sql = "SELECT `username`, `password` FROM `customer` WHERE username='$username' AND password=PASSWORD('$password')";
+            
+            // run mysql query and store results to $result
+            if ($result = mysqli_query($conn, $sql)) {
+                // has at least 1 result
+                if (mysqli_num_rows($result) > 0) {
+                    // add customer to session
+                    $_SESSION['Customer'] = $username;
+
+                    // Free result set (free up memory)
+                    mysqli_free_result($result);
+
+                    // redirects to index.php upon successful login
+                    header("Location: ./index.php");
+                    exit;
+                }
+                else {
+                    $invalid_login = true;
+                    $error_message = "<span style='color: red'> Username or password is incorrect! </span>";
+                }
+            }
+            else {
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+            }
+        }
+        // fields empty
+        else if (empty($_POST['username']) && empty($_POST['password'])) {
+            $invalid_login = true;
+            $error_message = "<span style='color: red'> Fields are empty! </span>";
+        }
+        // username field is empty
+        else if (empty($_POST['username'])) {
+            $invalid_login = true;
+            $error_message = "<span style='color: red'> Please enter the username! </span>";
+        }
+        // password field is empty
+        else if (empty($_POST['password'])) {
+            $invalid_login = true;
+            $error_message = "<span style='color: red'> Please enter the password! </span>";
         }
     }
+    // ------------------------------------------- //
+    // --- END OF LOGIN AUTHENTICATION SECTION --- //
+    // ------------------------------------------- //
+
 }
 
 // debug print outs
