48de857ef2512ef306a801c7015f58b48d785c49
s3757317 <s3757317@users.noreply.github.com>
2020-12-06 00:19:48 +0700

    added admin validation, updated name regex

--- scripts/tools.php
+++ scripts/tools.php
@@ -59,6 +59,9 @@ $invalid_login = false;
 // signup error messages
 $first_name_valid = $last_name_valid = $email_valid = $phone_valid = $username_valid = $password_valid = false;
 $nameError = $emailError = $phoneError = $usernameError = $passwordError = "";
+// add admin error messages
+$flag_valid = false;
+$flagError = "";
 
 // when a POST form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -157,7 +160,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // --------------------------------- //
     // --- SIGNUP VALIDATION SECTION --- //
     // --------------------------------- //
-    else if (isset($_POST['signup'])) {
+    // sign up or add customer from admin management
+    else if (isset($_POST['signup']) || isset($_POST['add-customer']) ) {
         // Validate first name
         if (empty($_POST["first_name"])) {
             $nameError = "First Name is required!";
@@ -165,7 +169,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (isset($_POST["first_name"])) {
                 $first_name = test_input($_POST["first_name"]);
                 // not matching regex, format error message
-                if (!preg_match("/^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/", $first_name)) {
+                if (!preg_match("/^[A-Za-z]+$/", $first_name)) {
                     $nameError = "Please enter an appropriate name!";
                 } else {  
                     $first_name_valid = true;
@@ -180,7 +184,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (isset($_POST["last_name"])) {
                 $last_name = test_input($_POST["last_name"]);
                 // not matching regex, format error message
-                if (!preg_match("/^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/", $last_name)) {
+                if (!preg_match("/^[A-Za-z]+$/", $last_name)) {
                     $nameError = "Please enter an appropriate name!";
                 } else {  
                     $last_name_valid = true;
@@ -260,7 +264,139 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($first_name_valid && $last_name_valid && $email_valid && $phone_valid && $username_valid && $password_valid) {
             $sql = "INSERT INTO `customer`(`last_name`, `first_name`, `email`, `phone_number`, `username`, `password`) VALUES ('$last_name', '$first_name', '$email', '$phone','$username', PASSWORD('$password'))";
             $conn->query($sql);
-            header("Location: index.php?=home");
+            if (isset($_SESSION['Admin'])) {
+                Header("Location: index.php?p=admin&c=users");
+                exit();
+            }
+            else {
+                Header("Location: index.php?=home");
+                exit();
+            }
+        }
+    }
+
+    // add admin from admin management
+    else if (isset($_POST['add-admin'])) {
+        // Validate first name
+        if (empty($_POST["first_name"])) {
+            $nameError = "First Name is required!";
+        } else {
+            if (isset($_POST["first_name"])) {
+                $first_name = test_input($_POST["first_name"]);
+                // not matching regex, format error message
+                if (!preg_match("/^[A-Za-z]+$/", $first_name)) {
+                    $nameError = "Please enter an appropriate name!";
+                } else {  
+                    $first_name_valid = true;
+                }
+            }
+        }
+
+        // Validate last name
+        if (empty($_POST["last_name"])) {
+            $nameError = "Last Name is required!";
+        } else {
+            if (isset($_POST["last_name"])) {
+                $last_name = test_input($_POST["last_name"]);
+                // not matching regex, format error message
+                if (!preg_match("/^[A-Za-z]+$/", $last_name)) {
+                    $nameError = "Please enter an appropriate name!";
+                } else {  
+                    $last_name_valid = true;
+                }
+            }
+        }
+
+        // Validate email
+        if (empty($_POST["email"])) {
+            $emailError = "Email is required!";
+        } else {
+            if (isset($_POST["email"])) {
+                if (!empty($_POST["email"])) {
+                    $email = test_input($_POST["email"]);
+                    // check if e-mail address is well-formed
+                    if (!is_valid_email($email)) {
+                        $emailError = "Invalid email format!";
+                    } else {
+                        $email_valid = true;
+                    }
+                }
+            }
+        }
+
+        // Validate phone number
+        if (empty($_POST["phone"])) {
+            $phoneError = "Phone number is required!";
+        } else {
+        // field not empty, check if data is not null 
+        if (isset($_POST["phone"])) {
+                $phone = test_input($_POST["phone"]);
+                // not matching regex, format error message
+                if (!preg_match("/^\+?\d{0,13}/", $phone)) {
+                    $phoneError = "Invalid phone number";
+                } else {
+                    $phone_valid = true;
+                }
+            }
+        }
+
+        // Validate username
+        if (empty($_POST["username"])) {
+            $usernameError = "Username is required!";
+        } else {
+            // field not empty, check if data is not null 
+            if (isset($_POST["username"])) {
+                $username = test_input($_POST["username"]);
+                
+                $sql = "SELECT `username` FROM `admin` WHERE username='$username'";
+                
+                // run mysql query and store results to $result
+                if ($result = mysqli_query($conn, $sql)) {
+                    // has at least 1 result
+                    if (mysqli_num_rows($result) > 0) {
+                        $usernameError = "Username has been taken!";
+                    } else {
+                        $username_valid = true;
+                    }
+                }
+                else {
+                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+                }
+            }
+        }
+
+        // Validate password
+        if (empty($_POST["password"])) {
+            $passwordError = "Password is required!";
+        } else {
+            // field not empty, check if data is not null
+            if (isset($_POST["password"])) {
+                $password = test_input($_POST["password"]);
+                $password_valid = true;
+            }
+        }
+
+        $flags = [ROOT_ADMIN, LIST_CUSTOMERS, ADD_CUSTOMERS, EDIT_CUSTOMERS, DELETE_CUSTOMERS, LIST_ADMINS, ADD_ADMINS, EDIT_ADMINS, DELETE_ADMINS];
+        // Validate flag
+        if (empty($_POST["flag"])) {
+            $flagError = "Flag is required!";
+        } else {
+            // field not empty, check if data is not null
+            if (isset($_POST["flag"])) {
+                $flag = test_input($_POST["flag"]);
+                // check if flag is in the flag list
+                if (in_array($flag, $flags))
+                    $flag_valid = true;
+                else
+                    $flagError = "Invalid flag!";
+            }
+
+        }
+
+        if ($first_name_valid && $last_name_valid && $email_valid && $phone_valid && $username_valid && $password_valid && $flag_valid) {
+            $sql = "INSERT INTO `admin`(`last_name`, `first_name`, `email`, `phone_number`, `username`, `password`, `flag`) VALUES ('$last_name', '$first_name', '$email', '$phone','$username', PASSWORD('$password'), '$flag')";
+            $conn->query($sql);
+            Header("Location: index.php?p=admin&c=admins");
             exit();
         }
     }
@@ -277,9 +413,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // prePrintArray($_FILES);
 // echo '$_GET array';
 // prePrintArray($_GET);
-// echo '$_POST array';
-// prePrintArray($_POST);
-// echo '$_SESSION array';
-// prePrintArray($_SESSION);
+echo '$_POST array';
+prePrintArray($_POST);
+echo '$_SESSION array';
+prePrintArray($_SESSION);
 
 ?>
