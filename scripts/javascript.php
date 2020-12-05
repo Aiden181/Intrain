7c3d31501063884b7d2540e69d4e7ba39ae7d092
s3757317 <s3757317@users.noreply.github.com>
2020-12-05 23:12:21 +0700

    javascript login authentication and signup validation

--- scripts/javascript.php
+++ scripts/javascript.php
@@ -3,42 +3,102 @@
         window.history.back();
     }
     
-    function checkLogin() {
-        // var username = password = "";
-        // e.preventDefault();
-        // return false;
+    function checkLogin(e) {
+        var username = password = "";
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        //validate username
+        if (username == "") {
+            alert("Please fill in your username");
+            e.preventDefault();
+            return false;
+        } else {
+            return true;
+        }
+
+        //validate password
+        if (password == "") {
+            alert("Please fill in your password");
+            e.preventDefault();
+            return false;
+        } else {
+            return true;
+        }
     }
     
-    function checkSignup() {
-        // var last_name = first_name = email = phone = username = password = "";
-        // e.preventDefault();
-        // return false;
+    function checkSignup(e) {
+        var last_name = first_name = email = phone = username = password = "";
+        var last_name = document.getElementById('last_name').value;
+        var first_name = document.getElementById('first_name').value;
+        var email = document.getElementById('email').value;
+        var phone = document.getElementById('phone').value;
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        var name_regex = /^[A-Za-z]+$/;
+        var email_regex =/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/;
+        var phone_regex =/^\+?\d{0,13}/s;
+        var password_regex=/^(?=.{4,20}$)(?:[a-zA-Z\d]+(?:(?:\.|-|_)[a-zA-Z\d])*)+$/;
+        //validate name
+        if (first_name == ""  || last_name == "") {
+            alert("Please fill in your name");
+            e.preventDefault();
+            return false;
+        } else if (name_regex.test(first_name) == false || name_regex.test(last_name) == false) {
+            alert("your name should be in alphabet");
+            e.preventDefault();
+            return false;
+        } else {
+            return true;
+        }
+
+        //validate email
+        if (email == "") {
+            alert("Please fill in your email");
+            e.preventDefault();
+            return false;
+        } else if (email_regex.test(email)== false ) {
+            alert("Invalid email format!");
+            e.preventDefault();
+            return false;
+        } else {
+            return true;
+        }
+
+        //validate phone
+        if (phone == "") {
+            alert("Please fill in your phone number");
+            e.preventDefault();
+            return false;
+        } else if (phone_regex.test(phone) == false ) {
+            alert("Invalid phone number");
+            e.preventDefault();
+            return false;
+        } else {
+            return true;
+        }
+
+        //validate username
+        if (username == "") {
+            alert("Please fill in your username");
+            e.preventDefault();
+            return false;
+        } else {
+            return true;
+        }
+
+        //validate password
+        if (password == "") {
+            alert("Please fill in your password");
+            e.preventDefault();
+            return false;
+        // } else if (password_regex.test(password) == false ) {
+        //     alert("Password should be atleast 4 characters and have atleast one capital letter");
+        //     e.preventDefault();
+        //     return false;
+        } else {
+            return true;
+        }
     }
-    
-    // $(function register() {
-    //     $('form').on('submit', function(e) {
-    //         // e.preventDefault();
-
-    //         // var name = $('#name').val();
-    //         // var email = $('#email').val();
-    //         // var password = $('#password').val();
-    //         // $.ajax({
-    //         //     type: "POST",
-    //         //     url: "register.php",
-    //         //     data: {
-
-    //         //         "name": name,
-    //         //         "email": email,
-    //         //         "password": password
-    //         //     },
-    //         //     success: function(data) {
-    //         //         $('.result').php(data);
-    //         //         $('#customer-signup')[0].reset();
-    //         //         alert("Account registered")
-    //         //     }
-    //         // });
-    //     });
-    // });
 
     function UpdateCheckBox(tgl, start, stop)
     {
