3da04d8b9fc6c35ebff6452e1e59fd93ce53109a
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-12-06 15:04:50 +0700

    Nutrition Facts Update (Not Complete Yet)

--- css/nutrition-facts-style.php
+++ css/nutrition-facts-style.php
@@ -5,8 +5,15 @@
 /*Major Content*/
 #left-content{
     position: relative;
+    left: 15px;
     top: 50px;
-    padding-top: 50px;
+    padding: 50px;
+}
+
+#left-content-2{
+    position: relative;
+    left: 50px;
+    top: -480px;
 }
 
 #left-content p, h2{
@@ -14,16 +21,38 @@
     text-align: justify;
 }
 
+#left-img{
+    position: relative;
+    right: 590px;
+    bottom: 360px;
+}
+
 #right-content{
     position: relative;
-    top: 90px;
-    padding-left: 590px;
-    padding-top: 50px
+    left: 70px;
+    top: -170px;
+}
+
+#right-content-2{
+    position: relative;
+    left: 35%;
+    bottom: 400px;
+}
+
+#right-content-2 p , h2{
+    width: 480px;
+    text-align: justify;
 }
 
 #right-content p, h2{
-    width: 480px;
+    width: 320px;
     text-align: justify;
+}
+
+#right-img{
+    position: relative;
+    bottom: 260px;
+
 }
 
 /* footer spacing */
--- pages/nutrition.php
+++ pages/nutrition.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="Description" content="Enter your description here" />
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -20,7 +20,7 @@
 
     <!-- website icon -->
     <link rel="icon" href="./images/Intrain.png">
-    
+
     <title>Nutrition</title>
 </head>
 
@@ -28,65 +28,50 @@
     <!--Section 2-->
     <section>
         <div class="container-sm" id="left-content">
-            <h2>Title</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                into
-                electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of
-                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <h2>You Don’t Need to Eat Every 2–3 Hours</h2>
+            <p>Some people believe that having smaller, more frequent meals may help them lose weight.
+                However, some studies suggest that meal size and frequency have no effect on fat burning or body weight.
+            </p>
+            <img id="right-img" src="/images/nutrition-1.jpg" style="width: 320px; height: 320px;" alt="">
         </div>
     </section>
 
     <!--Section 3-->
     <section>
         <div class="container-sm" id="right-content">
-            <h2>Title</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                into
-                electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of
-                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <h2>Eggs Are One of the Healthiest Foods You Can Eat</h2>
+            <p>Eggs have been unfairly demonized because their yolks are high in cholesterol.
+                However, studies show that cholesterol from eggs doesn’t raise blood cholesterol in the majority of
+                people.
+            </p>
+            <img id="left-img" src="/images/nutrition-2.jpg" style="width: 480px; height: 340px;" alt="">
         </div>
     </section>
 
     <!--Section 4-->
     <section>
-        <div class="container-sm" id="left-content">
-            <h2>Title</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                into
-                electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of
-                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div class="container-sm" id="left-content-2">
+            <h2>Weight Loss Supplements Rarely Work</h2>
+            <p>There are many different weight loss supplements on the market — and they almost never work.
+                They’re claimed to lead to magical results but fail when put to the test in studies.
+                Even for the few that work — like glucomannan — the effect is too small to really make a noticeable
+                difference.
+            </p>
         </div>
+        <img src="/images/nutrition-3.jpg" alt="">
     </section>
 
     <!--Section 5-->
     <section>
-        <div class="container-sm" id="right-content">
-            <h2>Title</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                into
-                electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of
-                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div class="container-sm" id="right-content-2">
+            <h2>Fruit Juice Isn’t That Different From Sugary Soft Drinks
+            </h2>
+            <p>Many people believe that fruit juices are healthy, as they come from fruit.
+                Though fresh fruit juice may provide some of the antioxidants found in fruit, it contains just as much
+                sugar as sugary soft drinks like Coca-Cola.
+            </p>
         </div>
+        <img src="/images/nutrition-4.jpg" alt="">
     </section>
 </body>
 
