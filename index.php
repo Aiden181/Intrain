43bda8fd3ace0d35a2613c45bba19f121106772d
s3757317 <s3757317@users.noreply.github.com>
2020-12-10 01:22:48 +0700

    moved POST handler to separate file+converted to using Database class

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ include_once 'init.php';
 include_once(INCLUDES_PATH . "/Database.php");
 include_once(INCLUDES_PATH . "/db-connect.php");
 include_once(SCRIPT_PATH . "/tools.php");
+include_once(SCRIPT_PATH . '/post-request.php');
 include_once(SCRIPT_PATH . "/javascript.php");
 include_once(INCLUDES_PATH . "/page-builder.php");
 ?>
