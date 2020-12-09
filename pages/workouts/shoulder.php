e8fc3144f6577c255d0d9530af2d481e568a8c21
Aiden181 <53722553+Aiden181@users.noreply.github.com>
2020-12-09 20:20:16 +0700

    Upper Body Workout Pages Videos & Content Upload

--- pages/workouts/shoulder.php
+++ pages/workouts/shoulder.php
@@ -24,6 +24,100 @@
 </head>
 
 <body>
+    <div class="container-sm">
+        <div class="row">
+            <!--Section 1 -->
+            <section class="col" id="first-section">
+                <h2><b>Decline Push-Up</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Decline Push-Up.mp4'?>" width="1000px" height="500px"
+                    controls></video>
+                <div class="container" id="content">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Shoulder-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="width: 350px; position: relative; bottom: 350px; left: 280px;">
+                        <ol id="instruction">
+                            <li>Use a bench or any item to lift your feet.</li>
+                            <li>Place your hands slightly wider than your shoulders.
+                            </li>
+                            <li>Slowly and steadily lower your body as much as you can.</li>
+                            <li>Raise your body back until original position.</li>
+                        </ol>
+                    </div>
+                </div>
+
+            </section>
+
+            <!--Section 2 -->
+            <section class="col">
+                <h2><b>Side Lateral Raises</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Side Lateral Raises.mp4'?>" width="1000px" height="500px"
+                    controls></video>
+                <div class="container" id="content">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Shoulder-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="width: 320px; position: relative; bottom: 400px; left: 250px;">
+                        <ol id="instruction">
+                            <li>Stand with your back straight with dumbbells, palms rotates to your hips.</li>
+                            <li>Raise your arms with a little bend by your elbow as high as you can.</li>
+                            <li>Slowly relax your arms to reach the original position.</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+
+            <!--Section 3 -->
+            <section class="col">
+                <h2><b>Bench Dips</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Bench Dips.mp4'?>" width="1000px" height="500px"
+                    controls></video>
+                <div class="container" id="content">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Shoulder-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="width: 320px; position: relative; bottom: 400px; left: 250px;">
+                        <ol id="instruction">
+                            <li>Hold the edge of anything that can support your weight with your hands, Keep your feet
+                                together and legs straight as you can.</li>
+                            <li>Lower your body as much as you can.</li>
+                            <li>Steadily push back up to the original straight position.</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+
+            <!--Section 4 -->
+            <section class="col">
+                <h2 style="width: 510px;"><b>Seated Dumbbell Shoulder Press</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Seated Dumbbell Shoulder Press.mp4'?>" width="1000px" height="500px"
+                    controls></video>
+                <div class="container" id="content">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Shoulder-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="width: 320px; position: relative; bottom: 400px; left: 250px;">
+                        <ol id="instruction">
+                            <li>Grab a dumbbell or two while standing upright.</li>
+                            <li>Raise the dumbbell with your arms being fully straight as hight as you can until it eye
+                                level.
+                            </li>
+                            <li>Lower the dumbbell in a slow manner to the original position then repeat.</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+
+            <!--Section 5 -->
+            <section class="col">
+                <h2><b>Front Raises</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Front Raises.mp4'?>" width="1000px" height="500px"
+                    controls></video>
+                <div class="container" id="last-section">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Shoulder-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="width: 320px; position: relative; bottom: 400px; left: 250px;">
+                        <ol id="instruction">
+                            <li>Sit on a chair or bench. Raise the dumbbells with your shoulders as high and verticle as
+                                you can.</li>
+                            <li>Lower the dumbbells back to return to the original position.</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
 </body>
 
 </html>
--- pages/workouts/triceps.php
+++ pages/workouts/triceps.php
@@ -24,6 +24,83 @@
 </head>
 
 <body>
+    <div class="container-sm">
+        <div class="row">
+            <!--Section 1 -->
+            <section class="col" id="first-section">
+                <h2><b>Push Up</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Push Up.mp4'?>" width="1000px" height="500px" controls></video>
+                <div class="container" id="content">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Triceps-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="width: 320px; position: relative; bottom: 480px; left: 30px;">
+                        <ol id="instruction">
+                            <li>Place your hands firmly on the ground, directly under shoulders.</li>
+                            <li>Flatten your back so your entire body is straight, adjust your legs depending on the
+                                difficulties.</li>
+                            <li>Slowly move your shoulder blades up and down, remember to keep your elbows close to your
+                                body.</li>
+                            <li>Exhale when you push back to your original position.</li>
+                        </ol>
+                    </div>
+                </div>
+
+            </section>
+
+            <!--Section 2 -->
+            <section class="col">
+                <h2><b>Bench Dips</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Bench Dips.mp4'?>" width="1000px" height="500px"
+                    controls></video>
+                <div class="container" id="content">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Triceps-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="width: 320px; position: relative; bottom: 400px; left: 250px;">
+                        <ol id="instruction">
+                            <li>Hold the edge of anything that can support your weight with your hands, Keep your feet
+                                together and legs straight as you can.</li>
+                            <li>Lower your body as much as you can.</li>
+                            <li>Steadily push back up to the original straight position.</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+
+            <!--Section 3 -->
+            <section class="col">
+                <h2><b>Seated Triceps Extensions</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Seated Triceps Extensions.mp4'?>" width="1000px" height="500px"
+                    controls></video>
+                <div class="container" id="content">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Triceps-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="width: 320px; position: relative; bottom: 480px; left: 30px;">
+                        <ol id="instruction">
+                            <li>Stand facing bench or anything that can be sturdy to withstand your weight.</li>
+                            <li>Place your hands on edge of bench or the platform, slightly wider than your shoulder.
+                            </li>
+                            <li>Steadily lower your body until your chest almost touches the platform.</li>
+                            <li>Push your body until arms are completely straight.</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+
+            <!--Section 4 -->
+            <section class="col">
+                <h2><b>Elevated Pike Press</b></h2>
+                <video src="<?php echo VID_LOCATION . '/Elevated Pike Press.mp4'?>" width="1000px" height="500px"
+                    controls></video>
+                <div class="container" id="last-section">
+                    <img id="content-background" src="<?php echo IMG_LOCATION . '/Triceps-Background.jpg' ?>" alt="">
+                    <div class="text-block" style="position: relative; bottom: 540px; left: 180px">
+                        <ol id="instruction">
+                            <li>Use a bench or anything to support your feet off the ground.</li>
+                            <li>Lower your head as much as you can by using your elbows.</li>
+                            <li>Push hard to tense up your arms and neck to return to original position.</li>
+                        </ol>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
 </body>
 
 </html>
